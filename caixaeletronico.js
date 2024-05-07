7550ecbc0d4e718666e473defc11536546ecacfe
Eduardo Lima <eduardolimacrr@gmail.com>
2024-05-07 15:00:03 -0300

    dia-7

---
+++ caixaeletronico.js
@@ -0,0 +1,43 @@
+let saldo = 1000;
+let maiorValorInserido = 0;
+let somaValoresInseridos = 0;
+let totalTransacoes = 0;
+let continuar = false
+
+do { 
+    const nome = prompt('Insira seu nome: ');
+    const cpf = prompt('Digite seu cpf: ');
+    const valor = Number(prompt ('Digite o valor da transacao: '));
+    const operacao = prompt('Qual operacao deseja realizar? S/D')
+
+    if (valor < 0) {
+        console.log('Valor inválido. A transação nõo foi realizada.');
+    }else if (operacao == "S" && valor > saldo) {
+        console.log('Saldo insuficiente. A transação não foi realizada');
+    }else if(operacao == 'S'){
+        console.log(`Olá, ${nome} (${cpf}), seu saldo atual é R$${saldo}.`); 
+        saldo -= valor;
+        totalTransacoes++;
+        somaValoresInseridos += valor;
+        if (valor > maiorValorInserido ){
+            maiorValorInserido = valor;
+        }
+        console.log(`Transação realizada com sucesso. Seu saldo atual é R$${saldo}`);
+    }
+
+    const opcao =prompt('Deseja continuar? (1 para continuar, 2 para parar):')
+    if (opcao == '1'){
+        continuar = true
+    }else if (opcao === '2') {
+        continuar = false
+    }else {
+        console.log('Opção inválida. Programa encerrado.');
+        continuar = false 
+    }
+        
+    
+} while (continuar)
+
+console.log(`Saldo final: R$${saldo}`);
+console.log(`Maior valor inserido: R$${maiorValorInserido}`);
+console.log(`Média dos valores inseridos: R$${somaValoresInseridos / totalTransacoes}`);
